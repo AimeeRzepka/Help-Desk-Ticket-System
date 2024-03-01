805fd29ffca7c0e4b1386392c122ef5be9fb64b0
Benjamin Holmes <manbej.sholem@gmail.com>
2024-02-29 21:37:04 -0500

    It works 2 pog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { TicketsComponent } from './display_tickets/tickets.component';
+import { AddTicketComponent } from './add-ticket/add-ticket.component';
 
 export const routes: Routes = [{
     path: 'tickets',
     component: TicketsComponent
-},];
+},
+{
+    path: 'add-ticket',
+    component: AddTicketComponent
+},
+{
+    path: 'update-ticket/:id',
+    component: AddTicketComponent
+}
+
+];
